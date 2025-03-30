63867793936d8ec360b928def68b7611e80f47ae
Fighttttz <nantawut.buat@bumail.net>
2025-03-30 22:50:06 +0700

    เพิ่ม End Credit

--- Assets/Scripts/WinTrigger.cs
+++ Assets/Scripts/WinTrigger.cs
@@ -1,20 +1,12 @@
 using UnityEngine;
-using TMPro; 
+using UnityEngine.SceneManagement;
 public class WinTrigger : MonoBehaviour
 {
-    public GameObject winTextUI;
-
-    private void Start()
-    {
-        winTextUI.SetActive(false); // ซ่อน UI ตอนเริ่มเกม
-    }
-
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player")) // ตรวจสอบว่าเป็นผู้เล่นไหม
         {
-            winTextUI.SetActive(true); // แสดง UI เมื่อชนกับจุด
-            Debug.Log("You Win!");
+            SceneManager.LoadScene("Ending");
         }
     }
 }
